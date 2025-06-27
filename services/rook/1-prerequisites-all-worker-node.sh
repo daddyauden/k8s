37bb319a2f80748b1c72080122ea6762eb22779c
Daddy Auden <daddyauden@secstep.com>
2025-06-27 14:35:32 +0000

    update script for rook

--- services/rook/1-prerequisites-all-worker-node.sh
+++ services/rook/1-prerequisites-all-worker-node.sh
@@ -3,13 +3,14 @@ set -e
 
 ######## all worker node ###########
 ## check which disk
-lsblk -f
-blkid
 
 DISK1=sdc
 
 for device in $DISK1; do
     sudo sgdisk --zap-all /dev/$device
-    sudo dd if=/dev/zero of=/dev/$device bs=1M count=1000 oflag=direct,dsync
     sudo wipefs -a --force /dev/$device
+    sudo dd if=/dev/zero of=/dev/$device bs=1M count=1000 oflag=direct,dsync
 done
+
+lsblk -f
+blkid
