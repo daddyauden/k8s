b413ee583dd2f30fa92dd6896a67bc646e36a17f
Daddy Auden <daddyauden@secstep.com>
2025-08-01 15:45:06 -0400

    update master control plane host

--- install/1-setup.sh
+++ install/1-setup.sh
@@ -35,7 +35,7 @@ sudo systemctl restart containerd
 sudo systemctl enable containerd
 
 sudo apt-get update
-sudo apt install -y apt-transport-https ca-certificates curl gpg iptables ebtables socat conntrack
+sudo apt install -y apt-transport-https ca-certificates curl gpg iptables
 curl -fsSL https://pkgs.k8s.io/core:/stable:/${VERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
