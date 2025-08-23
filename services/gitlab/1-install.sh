3814b7b7a88a6278575fd5fb534a7ab7eb83d08f
Daddy Auden <daddyauden@secstep.com>
2025-08-23 17:17:38 +0000

    upgrade gitlab and runner

--- services/gitlab/1-install.sh
+++ services/gitlab/1-install.sh
@@ -3,23 +3,7 @@ set -e
 
 helm repo add gitlab http://charts.gitlab.io/
 
-kubectl apply -f - <<EOF
-apiVersion: v1
-kind: Secret
-metadata:
-  name: gitlab-object-storage
-type: Opaque
-stringData:
-  connection: |
-    provider: AWS
-    region: us-east-1
-    aws_access_key_id: ead36fa2a638f64ce5c74885
-    aws_secret_access_key: 5ef0ebfcb0fd30c02170fb6baba6f6fb82661836407f25751c5ccb13ae1aa0f4
-    endpoint: https://s3.$DOMAIN
-    path_style: true
-EOF
-
-helm install gitlab gitlab/gitlab --version 9.2.2 -f values.yaml --set global.shell.port=222 --set global.hosts.domain="$DOMAIN" --set global.hosts.gitlab.name="gitlab.$DOMAIN" --set global.hosts.pages.name="pages.$DOMAIN" --set global.hosts.ssh="gitlab.$DOMAIN"
+helm install gitlab gitlab/gitlab --version 9.3.0 -f values.yaml --set global.hosts.domain="$DOMAIN" --set global.hosts.gitlab.name="gitlab.$DOMAIN" --set global.hosts.minio.name="minio-gitlab.$DOMAIN" --set global.hosts.ssh="gitlab.$DOMAIN"
 
 # after update values.yaml, run it
-# helm upgrade gitlab gitlab/gitlab --install -f values.yaml --set global.shell.port=222 --set global.hosts.domain="$DOMAIN" --set global.hosts.gitlab.name="gitlab.$DOMAIN" --set global.hosts.pages.name="pages.$DOMAIN" --set global.hosts.ssh="gitlab.$DOMAIN"
+# helm upgrade gitlab gitlab/gitlab --install -f values.yaml --set global.hosts.domain="$DOMAIN" --set global.hosts.gitlab.name="gitlab.$DOMAIN" --set global.hosts.minio.name="minio-gitlab.$DOMAIN" --set global.hosts.ssh="gitlab.$DOMAIN"
--- services/gitlab/runner/1-install.sh
+++ services/gitlab/runner/1-install.sh
@@ -2,6 +2,6 @@
 
 helm repo add gitlab http://charts.gitlab.io/
 
-helm install gitlab-runner gitlab/gitlab-runner --version 0.79.1 -f values.yaml --set gitlabUrl="https://gitlab.$DOMAIN/" 
+helm install gitlab-runner gitlab/gitlab-runner --version 0.80.0 -f values.yaml --set gitlabUrl="https://gitlab.$DOMAIN/" 
 
 # helm upgrade gitlab-runner gitlab/gitlab-runner -f values.yaml --set gitlabUrl="https://gitlab.$DOMAIN/" 
